cd0e061844a2fdee4e890db943d43f2c3e193ad1
luiz.setragni <luiz.lelis_accenture@prestador.globo>
2024-04-24 00:03:40 -0300

    fix name endpoint

--- word_learner_api/word_learner_app/urls.py
+++ word_learner_api/word_learner_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import save_word
+from .views import crawl_and_save_words
 
 urlpatterns = [
-    path('save-word/', save_word, name='save_word'),
+    path('crawl_and_save_words/', crawl_and_save_words, name='crawl_save'),
 ]
