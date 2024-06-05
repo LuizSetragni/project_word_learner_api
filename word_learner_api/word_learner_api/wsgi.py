3ca6a8874eeb2fbc4a39f7d7e8511224a534730c
Luiz Setragni <luiz.lelis_accenture@prestador.globo>
2024-06-05 13:10:26 -0300

    add echo in build

--- word_learner_api/word_learner_api/wsgi.py
+++ word_learner_api/word_learner_api/wsgi.py
@@ -1,9 +1,14 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+print("Iniciando a aplicação WSGI...")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'word_learner_api.settings')
 
 application = get_wsgi_application()
 
+print("Aplicação WSGI iniciada com sucesso.")
+
 app = application
+
+print("A aplicação foi atribuída à variável 'app'.")
